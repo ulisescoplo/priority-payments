3cff15cfa6c05d6e6eaabbbe4ab362a828ab31e7
Ulises Coplo <ucoplo@optimous.com>
2023-08-17 19:52:54 +0200

    change error return

--- priority-payments/connector/priority-connector.js
+++ priority-payments/connector/priority-connector.js
@@ -15,8 +15,8 @@ class PriorityConnector {
     async doPost(relativeUrl, headers, data) {
         try {
             const response = await this.doRequest('POST', relativeUrl, headers, data);
-            console.log(response);
-            return { result: response.data };
+            
+            return response;
         } catch (error) {
             console.log('doPost'+error.message);
             //throw new Error('An error has occurred in Request.00');
@@ -24,21 +24,21 @@ class PriorityConnector {
     }
 
     async doRequest(method, relativeUrl, headers, data) {
-        headers = await this.addTokenToHeader(headers);
-        
-        const options = {
-            headers: headers,
-            //data: data
-        };
-
-        if (method.toUpperCase() === 'GET') {
-            options.params = data;
-         } else {
-            options.data = data;
-
-         } 
-
         try {
+            headers = await this.addTokenToHeader(headers);
+            
+            const options = {
+                headers: headers,
+                //data: data
+            };
+
+            if (method.toUpperCase() === 'GET') {
+                options.params = data;
+            } else {
+                options.data = data;
+
+            } 
+       
             const response = await this.client.request({
                 method: method,
                 url: this.endpointUrl + relativeUrl,
@@ -48,8 +48,11 @@ class PriorityConnector {
             return response.data;
         } catch (error) {
             console.log("doRequest Error: "+error.message);
-            console.log(error.response.data.errors[0].errors);
-            throw new Error("doRequest Error: "+error.message);
+            let message = (error.response.data.errors)?error.response.data.errors[0].errors:error.response.data;
+            console.log(message);
+            //throw new Error("doRequest Error: "+error.message);
+            return {'statusCode':error.response.status,'body':message}
+            
         }
     }
 
@@ -58,15 +61,14 @@ class PriorityConnector {
         try {
             const response = await this.doRequest('GET', relativeUrl, headers, data);
             return response;
-            //return { result: response };
         } catch (error) {
-            console.log(error);
-            throw new Error('An error has occurred in Request doGet.');
+            console.log(error.message);
+            //throw new Error('An error has occurred in Request doGet.');
         }
     }
 
     async checkToken() {
-        const tokenFilePath = '/tmp/token'; 
+        const tokenFilePath = 'token'; 
 
         if (!fs.existsSync(tokenFilePath) || (Date.now() - fs.statSync(tokenFilePath).mtimeMs) > 600000) {
             const body = { value: process.env.PRIORITY_API_KEY };
@@ -75,7 +77,7 @@ class PriorityConnector {
                 'Cache-Control': 'no-cache'
             };
 
-            try {
+            //try {
                 this.token = await this.client.request({
                     method: 'POST',
                     url: this.endpointUrl + '/security/v1/apiKey/authenticate',
@@ -86,15 +88,15 @@ class PriorityConnector {
                     fs.writeFileSync(tokenFilePath, token);
                     return token;
                   })
-                  .catch(function (error) {
-                    console.error(error.message);
-                    throw new Error('An error has occurred endpoint authenticate. '+error.message);
-                  });
+            //       .catch(function (error) {
+            //         console.error(error.message);
+            //         throw new Error('An error has occurred endpoint authenticate. '+error.message);
+            //       });
 
-            } catch (error) {
-                console.error(error.message);
-                throw new Error('An error has occurred getting the token.');
-            }
+            // } catch (error) {
+            //     console.error(error.message);
+            //     throw new Error('An error has occurred getting the token.');
+            // }
         } else {
             this.token = fs.readFileSync(tokenFilePath, 'utf8');
             return this.token;
