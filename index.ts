0ac6a56a54de28ab0f69f541bd0c8b05d3b9b61b
Ulises Coplo <ucoplo@optimous.com>
2023-08-17 12:48:12 +0200

    change index

--- index.ts
+++ index.ts
@@ -1,23 +1,8 @@
-//import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
-//import * as _ from 'lodash';
-
-//export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
 export const handler = async (event) => {
-  const max = 999;
-  const val = Math.random()*max;
-  console.log(`The random value (max ${max}) is: ${val}`);
+  // TODO implement
   const response = {
     statusCode: 200,
-    body: `The random value (max ${max}) is: ${val}`,
+    body: JSON.stringify('Hello Priority Payments!'),
   };
   return response;
 };
-
-// export const handler = async (event) => {
-//   // TODO implement
-//   const response = {
-//     statusCode: 200,
-//     body: JSON.stringify('Hello from Lambda!'),
-//   };
-//   return response;
-// };
